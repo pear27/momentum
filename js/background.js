ae487cd1b40576b49fb46eb2187b6a08ddd5e5dc
yoona <jinalks43@gmail.com>
2023-10-13 15:45:34 +0900

    add background images

--- js/background.js
+++ js/background.js
@@ -3,7 +3,10 @@ const images = [
     "changhui-lee-2dPWRb5R_RM-unsplash.jpg",
     "changhui-lee-O7dwrmkqXCg-unsplash.jpg",
     "changhui-lee-oZk492eAgPY-unsplash.jpg",
-    "changhui-lee-qX0eUetnjJ8-unsplash.jpg"
+    "changhui-lee-qX0eUetnjJ8-unsplash.jpg",
+    "changhui-lee-0e1CGu_tsw8-unsplash.jpg",
+    "changhui-lee-TBta6TvFoKM-unsplash.jpg",
+    "changhui-lee-g8ExaCImT5o-unsplash.jpg"
 ];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
